e49e1754e9b67cc3d0fb3a75fc5f69e9ca93f9bd
Devang <23aiml014@charusat.edu.in>
2025-05-17 19:29:53 +0530

    ok

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -148,7 +148,7 @@ const Hero = () => {
               <div className="absolute inset-16 md:inset-20 rounded-full overflow-hidden border-2 border-[#00FF7F]/30 flex items-center justify-center">
                 <img
                   src={dhairya}
-                  alt="Dhairya"
+                  alt="Devang"
                   className="w-[6rem] md:w-[8rem] object-cover"
                 />
               </div>
